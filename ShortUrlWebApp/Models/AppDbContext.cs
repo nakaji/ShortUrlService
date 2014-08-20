302021281e47c521412cb116fc8a9f0d47ac60ec
nakaji <nakaji126@gmail.com>
2014-08-21 02:37:38 +0900

    エンティティモデルを追加

---
+++ ShortUrlWebApp/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace ShortUrlWebApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection") { }
+
+        public DbSet<ShortUrl> ShortUrls;
+    }
+
+    public class ShortUrl
+    {
+        public int Id { get; set; }
+        public string Original { get; set; }
+        public string User { get; set; }
+        public string Hash { get; set; }
+    }
+}
