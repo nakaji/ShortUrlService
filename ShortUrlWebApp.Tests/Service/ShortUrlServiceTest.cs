f8b279019a56f8300bdaff76ac22184f06a35014
nakaji <nakaji126@gmail.com>
2014-08-23 11:49:37 +0900

    テストを追加

--- ShortUrlWebApp.Tests/Service/ShortUrlServiceTest.cs
+++ ShortUrlWebApp.Tests/Service/ShortUrlServiceTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShortUrlWebApp.Models;
 using ShortUrlWebApp.Service;
 
 namespace ShortUrlWebApp.Tests.Service
@@ -14,6 +16,29 @@ namespace ShortUrlWebApp.Tests.Service
             var actual = sut.RegisterUrl("http://nakaji.hatenablog.com/");
 
             Assert.AreEqual("http://nkd.jp/129f5f", actual.Short);
+        }
+
+        [TestMethod]
+        public void 同じUrlを受け取ると既に登録されているものを返す()
+        {
+            var db = new AppDbContext();
+
+            db.ShortUrls.RemoveRange(db.ShortUrls.ToList());
+
+            db.ShortUrls.Add(new ShortUrl()
+            {
+                Original = "http://nakaji.hatenablog.com/",
+                Short = "http://nkd.jp/129f5f",
+                Hash = "129f5f"
+            });
+            db.SaveChanges();
+
+            var sut = new ShortUrlService();
+
+            var actual = sut.RegisterUrl("http://nakaji.hatenablog.com/");
+
+            Assert.AreEqual("http://nkd.jp/129f5f", actual.Short);
+            Assert.AreEqual(1, db.ShortUrls.Count());
         }
     }
 }
