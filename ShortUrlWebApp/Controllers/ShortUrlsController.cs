1a5e97bdc11a2065707ceaef76791996a40693d1
nakaji <nakaji126@gmail.com>
2014-08-24 02:48:31 +0900

    不要なページを削除

--- ShortUrlWebApp/Controllers/ShortUrlsController.cs
+++ ShortUrlWebApp/Controllers/ShortUrlsController.cs
@@ -13,78 +13,9 @@ namespace ShortUrlWebApp.Controllers
         private AppDbContext db = new AppDbContext();
 
         // GET: ShortUrls
-        public async Task<ActionResult> Index()
+        public async Task<ActionResult> List()
         {
             return View(await db.ShortUrls.ToListAsync());
-        }
-
-        // GET: ShortUrls/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ShortUrl shortUrl = await db.ShortUrls.FindAsync(id);
-            if (shortUrl == null)
-            {
-                return HttpNotFound();
-            }
-            return View(shortUrl);
-        }
-
-        // GET: ShortUrls/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ShortUrls/Create
-        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
-        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "Id,Original,User,Hash")] ShortUrl shortUrl)
-        {
-            if (ModelState.IsValid)
-            {
-                db.ShortUrls.Add(shortUrl);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-
-            return View(shortUrl);
-        }
-
-        // GET: ShortUrls/Edit/5
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ShortUrl shortUrl = await db.ShortUrls.FindAsync(id);
-            if (shortUrl == null)
-            {
-                return HttpNotFound();
-            }
-            return View(shortUrl);
-        }
-
-        // POST: ShortUrls/Edit/5
-        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
-        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,Original,User,Hash")] ShortUrl shortUrl)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(shortUrl).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(shortUrl);
         }
 
         // GET: ShortUrls/Delete/5
@@ -110,7 +41,7 @@ namespace ShortUrlWebApp.Controllers
             ShortUrl shortUrl = await db.ShortUrls.FindAsync(id);
             db.ShortUrls.Remove(shortUrl);
             await db.SaveChangesAsync();
-            return RedirectToAction("Index");
+            return RedirectToAction("List");
         }
 
         // POST: ShortUrls/Register
@@ -132,7 +63,6 @@ namespace ShortUrlWebApp.Controllers
 
             return new RedirectResult(shortUrl.Original);
         }
-
 
         protected override void Dispose(bool disposing)
         {
