0b122a94d44949bdf6318409d0741333f49fbfd7
nakaji <nakaji126@gmail.com>
2014-08-23 12:22:38 +0900

    テストを修正

--- ShortUrlWebApp.Tests/Service/ShortUrlServiceTest.cs
+++ ShortUrlWebApp.Tests/Service/ShortUrlServiceTest.cs
@@ -8,11 +8,31 @@ namespace ShortUrlWebApp.Tests.Service
     [TestClass]
     public class ShortUrlServiceTest
     {
+        private ShortUrlService sut;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            sut = new ShortUrlService();
+
+            using (var db = new AppDbContext())
+            {
+
+                db.ShortUrls.RemoveRange(db.ShortUrls.ToList());
+
+                db.ShortUrls.Add(new ShortUrl()
+                {
+                    Original = "http://nakaji.hatenablog.com/",
+                    Short = "http://nkd.jp/129f5f",
+                    Hash = "129f5f"
+                });
+                db.SaveChanges();
+            }
+        }
+
         [TestMethod]
         public void Urlを受け取るとShortUrlオブジェクトを返す()
         {
-            var sut = new ShortUrlService();
-
             var actual = sut.RegisterUrl("http://nakaji.hatenablog.com/");
 
             Assert.AreEqual("http://nkd.jp/129f5f", actual.Short);
@@ -22,18 +42,6 @@ namespace ShortUrlWebApp.Tests.Service
         public void 同じUrlを受け取ると既に登録されているものを返す()
         {
             var db = new AppDbContext();
-
-            db.ShortUrls.RemoveRange(db.ShortUrls.ToList());
-
-            db.ShortUrls.Add(new ShortUrl()
-            {
-                Original = "http://nakaji.hatenablog.com/",
-                Short = "http://nkd.jp/129f5f",
-                Hash = "129f5f"
-            });
-            db.SaveChanges();
-
-            var sut = new ShortUrlService();
 
             var actual = sut.RegisterUrl("http://nakaji.hatenablog.com/");
 
